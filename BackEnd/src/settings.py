d7a2a21f2cb6bec452c5393ae5682163c55fd1b3
ratte2d3d <kinoyama001@gmail.com>
2025-03-18 03:43:38 +0900

    Update settings.py

--- BackEnd/src/settings.py
+++ BackEnd/src/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+ALLOWED_HOSTS = ['main-8r55.onrender.com', 'localhost', '127.0.0.1']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_CREDENTIALS = True
