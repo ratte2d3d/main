799ea3b01d0ff6e93715e6ebbf1f34111a2b2a4c
ratte2d3d <kinoyama001@gmail.com>
2025-03-19 02:43:45 +0900

    全てのURLをindex.htmlにリダイレクトするよう修正(BackEnd側)

--- BackEnd/catan/urls.py
+++ BackEnd/catan/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path, include,re_path
 from rest_framework import routers
 from .views import GameResultViewSet, PersonalResultViewSet, PlayerViewSet
-from django.urls import re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
