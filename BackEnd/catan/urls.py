6a2c7e6c5e445e3797682f8773fb2889aa646666
ratte2d3d <kinoyama001@gmail.com>
2025-03-19 01:24:04 +0900

    静的ファイルのルーティング

--- BackEnd/catan/urls.py
+++ BackEnd/catan/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import GameResultViewSet, PersonalResultViewSet, PlayerViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'player', PlayerViewSet)
@@ -10,3 +12,6 @@ router.register(r'personalResult', PersonalResultViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
