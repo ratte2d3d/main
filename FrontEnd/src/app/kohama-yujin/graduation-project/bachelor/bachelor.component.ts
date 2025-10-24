999635274aad427a464dedf588a7470b62a656ad
ratte2d3d <kinoyama001@gmail.com>
2025-10-25 02:09:09 +0900

    学部卒業研究ページ追加

--- FrontEnd/src/app/kohama-yujin/graduation-project/bachelor/bachelor.component.ts
+++ FrontEnd/src/app/kohama-yujin/graduation-project/bachelor/bachelor.component.ts
@@ -8,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class BachelorComponent {
 
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  scrollToAbstract() {
+    const section = document.querySelector('.abstract');
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToBackground() {
+    const section = document.querySelector('.background');
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToRole() {
+    const section = document.querySelector('.role');
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToMethod() {
+    const section = document.querySelector('.method');
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToAchievement() {
+    const section = document.querySelector('.achievement');
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToTechnique() {
+    const section = document.querySelector('.technique');
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
--- FrontEnd/src/app/kohama-yujin/graduation-project/graduation-project.component.ts
+++ FrontEnd/src/app/kohama-yujin/graduation-project/graduation-project.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { coreImports } from '../../../core/core.imports';
 import { BachelorComponent } from './bachelor/bachelor.component';
 
@@ -9,5 +9,11 @@ import { BachelorComponent } from './bachelor/bachelor.component';
   styleUrl: './graduation-project.component.scss',
 })
 export class GraduationProjectComponent {
+  pageIndex = 0;
 
+  @ViewChild('bachelor') bachelor!: BachelorComponent;
+
+  onTabChange(event: any) {
+    this.pageIndex = event.index;
+  }
 }
