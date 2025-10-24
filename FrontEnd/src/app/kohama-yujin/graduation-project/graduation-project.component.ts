079fb571a3c1a1a457fe3060b9dadf99289a55e2
ratte2d3d <kinoyama001@gmail.com>
2025-10-24 16:57:43 +0900

    卒研ページ追加

--- FrontEnd/src/app/kohama-yujin/graduation-project/graduation-project.component.ts
+++ FrontEnd/src/app/kohama-yujin/graduation-project/graduation-project.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { coreImports } from '../../../core/core.imports';
 
 @Component({
   selector: 'app-graduation-project.component',
-  imports: [],
+  imports: [...coreImports],
   templateUrl: './graduation-project.component.html',
   styleUrl: './graduation-project.component.scss',
 })
