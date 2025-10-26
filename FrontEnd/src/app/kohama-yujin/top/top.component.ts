28e18004fbd7ba553acf327a4d88ee6b7b6f2403
ratte2d3d <kinoyama001@gmail.com>
2025-10-27 08:43:17 +0900

    ホーム編集

--- FrontEnd/src/app/kohama-yujin/top/top.component.ts
+++ FrontEnd/src/app/kohama-yujin/top/top.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { coreImports } from '../../../core/core.imports';
 
 @Component({
   selector: 'app-top',
-  imports: [],
+  imports: [...coreImports],
   templateUrl: './top.component.html',
   styleUrl: './top.component.scss',
 })
 export class TopComponent {
-
+  scroll(className: string) {
+    const section = document.querySelector(`.${className}`);
+    section?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
